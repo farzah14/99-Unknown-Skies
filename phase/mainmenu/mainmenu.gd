0a0e564eb4a716e1615e89a1825c9daa7c77af26
bknsehan <sechanridho.h@gmail.com>
2024-05-01 20:19:23 +0700

    Add prototype for settings

--- phase/mainmenu/mainmenu.gd
+++ phase/mainmenu/mainmenu.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 func _on_settings_pressed():
 	print("Telah Di Press")
-	pass # Replace with function body.
+	if $ui/settings.visible == true:
+		$ui/settings.visible = false
+	else:
+		$ui/settings.visible = true
+	pass
 
 func _on_exit_pressed():
 	get_tree().quit()
